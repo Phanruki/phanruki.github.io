ff05273d59c3b08e98f1f43ae8f5f68a3bf59860
Phanruki <phanruki@gmail.com>
2025-01-10 15:53:25 -0600

    Update: Improvement

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,29 +4,44 @@ window.onload = () => {
     $('.loader').addClass('hidden')
 };
 
-/* Style creator function */
-function createStyle (css){
-    $('<style>')
-    .prop('type', 'text/css')
-    .html(css)
-    .appendTo('head');
+/* Triggers */
+const pink = document.getElementById('version--pink--trigger')
+const white = document.getElementById('version--white--trigger')
+const dark = document.getElementById('version--dark--trigger')
+
+/* Change color functions */
+/* pink.onclick = () => {
+    $('body').removeClass()
+    $('body').addClass('version--pink')
 }
 
-/* Change style function */
-function chageStyle(id, css) {
-    $(id).click(()=> {
-        createStyle(css)
-    })
+white.onclick = () => {
+    $('body').removeClass()
+    $('body').addClass('version--white')
 }
 
-chageStyle('#a', '.title--header { color: var(--pink--light); }')
+dark.onclick = () => {
+    $('body').removeClass()
+} */
 
-/* Add header style */
-createStyle('.title--header { color: var(--white); }, .title--header {display:flex} ')
-
+/* Classes added by jQuery */
 $('.header__row__text a').addClass('title--header')
 
 
 
+/* Option 2 */
 
+function changeTheme(theme) {
+    $('body').removeClass()
+    $('body').addClass(theme)
+}
 
+document.getElementById('version--pink--trigger').onclick = () => {
+    changeTheme('version--pink')
+}
+document.getElementById('version--white--trigger').onclick = () => {
+    changeTheme('version--white')
+}
+document.getElementById('version--dark--trigger').onclick = () => {
+    changeTheme()
+} 
